27ef1a8f9111288d2a0ba246017069f9af161a88
Juliet Oma <julieto2001@gmail.com>
2025-03-19 09:53:42 +0100

    reverted template entry pointer

--- blogapp/src/pages/api/add-comment.ts
+++ blogapp/src/pages/api/add-comment.ts
@@ -1,7 +1,17 @@
 import { bcms } from "../../bcms-client";
+import type { CommentEntry } from "../../../bcms/types/ts";
 
+export async function GET(req:any, res:any){
+    try{
+        const blogs = (await bcms.entry.getAll("comment")) as CommentEntry[];
+        return res.status(200).json({success:true, data:blogs});
+    } catch(error){
+        console.error('Error in retrieving blogs:', error);
+        return res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+}
 
-export async function post(req: { body: { display_name: any; comment_text: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { success: boolean; message: string; }): any; new(): any; }; }; }) {
+export async function POST(req: { body: { display_name: any; comment_text: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { success: boolean; message: string; }): any; new(): any; }; }; }) {
     try {
         const { display_name, comment_text } = req.body;
 
@@ -10,9 +20,9 @@ export async function post(req: { body: { display_name: any; comment_text: any; 
         }
 
         // Create the new comment entry
-        const newComment = await bcms.entry.create('comment-template', {
-            meta: [{ lng: 'en', data: { display_name, comment_text } }],
-            statuses: [], // Set appropriate status if needed
+        const newComment = await bcms.entry.create('comment', {
+            meta: [{ lng: 'en', data: { display_name: "Oma", comment_text: "I love this!" } }],
+            statuses: [],
             content: [],
         });
 
